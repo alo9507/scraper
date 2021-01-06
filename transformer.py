d057a6aff405ffbb23d37eabd2bbcab23a15a95b
Andrew <alo42@georgetown.edu>
2021-01-06 11:12:22 -0500

    adds csv reading capabilities

--- transformer.py
+++ transformer.py
@@ -1,5 +1,8 @@
 import boto3
 from decouple import config
+import codecs
+import csv
+import pandas as pd
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
@@ -29,20 +32,33 @@ def keyParser(key):
     date = dateParser(parts[0])
 
     body = ""
+    article_links = []
+    comments = []
+    shares = []
+    reactions = []
+
+    article_object=s3_resource.Object('prop-watch-raw', key)
+
     if('.txt' in parts[3]):
-        article_object=s3_resource.Object('prop-watch-raw', key)
         body = article_object.get()['Body'].read().decode('utf-8') 
+    elif '.csv' in parts[3]:
+        for line in csv.DictReader(codecs.getreader("utf-8")(article_object.get()['Body'])):
+          print("sdfsdfds", line)
+        #   read_csv_from_s3('prop-watch-raw', )
+    elif 'like' in parts[4]:
+        for line in csv.DictReader(codecs.getreader("utf-8")(article_object.get()['Body'])):
+          print(line)
 
     article_data = {
         'articleId': parts[2], 
-        'reactions': { 'likes': [] },
-        'shares': [],
-        'comments': [],
+        'reactions': reactions,
+        'shares': shares,
+        'comments': comments,
         'year': date['year'],
         'month': date['month'],
         'day': date['day'],
         'publication': parts[1],
-        'article_link': "",
+        'article_links': article_links,
         'article_text': body
     }
     
@@ -54,6 +70,10 @@ pages = paginator.paginate(Bucket='prop-watch-raw')
 for page in pages:
     for obj in page['Contents']:
         json = keyParser(obj['Key'])
-        print(json)
         # save json to a new location in S3
     
+def read_csv_from_s3(bucket_name, key, column):
+    data = s3.get_object(Bucket=bucket_name, Key=key)
+
+    for row in csv.DictReader(codecs.getreader("utf-8")(data.get()['Body'])):
+        print(row[column])
